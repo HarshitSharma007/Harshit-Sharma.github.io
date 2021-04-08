3844cd8c91a5d8517317ad1614d2f3f89222c54b
harshittheone007 <harshittheone007@gmail.com>
2021-04-08 09:27:21 +0530

    t

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 
- const sbtn=document.getElementById("submit1");
+//  const sbtn=document.getElementById("submit1");
 
 
 
-sbtn.addEventListener("click",(e)=>{
-    e.preventDefault();
+// sbtn.addEventListener("click",(e)=>{
+//     e.preventDefault();
    
 
-const name1 =document.getElementById("fullname").value;
-const message =document.getElementById("message").value;
-const useremail =document.getElementById("email").value;
-    console.log(name1);
-console.log("dddd");
-console.log(message);
-console.log(useremail);
-Email.send({
-    Host : "smtp.mailtrap.io",
-    Username : "7ab76cac40c621",
-    Password : "09d8ac7f78d76f",
-    To : 'harshittheone008.com',
-    From :`${useremail}`,
-    Subject : "This is the subject",
-    Body : message
-}).then(
-  message => alert("message sent")
-)
-});
+// const name1 =document.getElementById("fullname").value;
+// const message =document.getElementById("message").value;
+// const useremail =document.getElementById("email").value;
+//     console.log(name1);
+// console.log("dddd");
+// console.log(message);
+// console.log(useremail);
+// Email.send({
+   
+//   SecureToken :  "f92db9f1-2956-497b-9269-2f76fae518df",
+//   To:"harshittheone007@gmail.com",
+//     From :useremail,
+//     Subject : name1,
+//     Body : message
+// }).then(
+//   message => alert("message sent")
+// )});
+
 
 
